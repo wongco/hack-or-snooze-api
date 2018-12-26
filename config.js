6d4c151036f17443b4aa751ba48c227e6e270b10
wongco <ginson@gmail.com>
2018-12-25 21:45:43 -0800

    refactor config file

--- config.js
+++ config.js
@@ -4,36 +4,21 @@
 require('dotenv').config();
 
 // pull db uri from .env
-let DB_URI = process.env.DB_URI;
+let DB_URI = process.env.DB_URI || 'postgresql:///hack-or-snooze';
+let BCRYPT_WORK_ROUNDS = +process.env.BCRYPT_WORK_ROUNDS || 12;
+let JWT_OPTIONS = { expiresIn: 60 * 60 * 24 * 7 };
 
-// if db uri is undefined, use local db
-if (!DB_URI) {
-  DB_URI = 'postgresql:///hack-or-snooze';
-}
-
-// if test environment is active use test db
+// if test environment is active
 if (process.env.NODE_ENV === 'test') {
   DB_URI = 'postgresql:///hack-or-snooze-test';
-}
-
-let BCRYPT_WORK_ROUNDS;
-if (process.env.NODE_ENV === 'test') {
   BCRYPT_WORK_ROUNDS = 1;
-} else {
-  BCRYPT_WORK_ROUNDS = +process.env.BCRYPT_WORK_ROUNDS || 12;
+  JWT_OPTIONS = {};
 }
 
 const SECRET_KEY = process.env.SECRET_KEY || 'test-env-secret';
 const SERVER_PORT = +process.env.SERVER_PORT || 3000;
 const USERS_LIST_LIMIT = +process.env.USERS_LIST_LIMIT || 25;
 const STORIES_LIST_LIMIT = +process.env.STORIES_LIST_LIMIT || 25;
-
-let JWT_OPTIONS;
-if (process.env.NODE_ENV === 'test') {
-  JWT_OPTIONS = {};
-} else {
-  JWT_OPTIONS = { expiresIn: 60 * 60 * 24 * 7 };
-}
 
 module.exports = {
   SECRET_KEY,
