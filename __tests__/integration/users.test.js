c23072ff0d7bbfbf58b954105439a2bf6368473a
wongco <ginson@gmail.com>
2018-12-25 18:21:51 -0800

    update users route int test for auth middleware

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -1,13 +1,23 @@
 /** Integration tests for users routes */
 process.env.NODE_ENV = 'test';
 
+// npm imports
+const jwt = require('jsonwebtoken');
 const request = require('supertest');
+
+// class models
 const User = require('../../models/User');
 const Story = require('../../models/Story');
 
 // app imports
 const app = require('../../app');
 const db = require('../../db');
+
+// import config info
+const { SECRET_KEY } = require('../../config');
+
+let bobToken;
+let jasToken;
 
 beforeEach(async () => {
   // delete any data created by prior tests
@@ -19,11 +29,15 @@ beforeEach(async () => {
     password: '123456'
   });
 
+  bobToken = jwt.sign({ username: 'bob' }, SECRET_KEY);
+
   await User.addUser({
     username: 'jas',
     name: 'Jason',
     password: '123456'
   });
+
+  jasToken = jwt.sign({ username: 'jas' }, SECRET_KEY);
 
   await Story.addStory({
     title: 'How to eat cookies.',
@@ -53,9 +67,9 @@ beforeEach(async () => {
     username: 'jas'
   });
 
+  // add first story to username jas favorites
   const stories = await Story.getStories({});
   const storyId = stories[0].storyId;
-
   await User.addFavorite('jas', storyId);
 });
 
@@ -63,6 +77,7 @@ describe('GET /users', async () => {
   it('Get all users succeeded with valid query string params', async () => {
     const response = await request(app)
       .get('/users')
+      .set({ Authorization: `Bearer ${bobToken}` })
       .query({ skip: '1', limit: '25' });
 
     const { users } = response.body;
@@ -72,7 +87,9 @@ describe('GET /users', async () => {
   });
 
   it('Get all users succeeded with no query string params', async () => {
-    const response = await request(app).get('/users');
+    const response = await request(app)
+      .get('/users')
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { users } = response.body;
     expect(response.statusCode).toBe(200);
@@ -83,17 +100,30 @@ describe('GET /users', async () => {
   it('Failed to get all users due to invalid query string params', async () => {
     const response = await request(app)
       .get('/users')
+      .set({ Authorization: `Bearer ${bobToken}` })
       .query({ skip: '1', limit: 'abc' });
 
     const { error } = response.body;
     expect(error.status).toBe(400);
     expect(error).toHaveProperty('title', 'Bad Request');
   });
+
+  it('Failed due to no auth token', async () => {
+    const response = await request(app)
+      .get('/users')
+      .query({ skip: '1', limit: 'abc' });
+
+    const { error } = response.body;
+    expect(error.status).toBe(401);
+    expect(error).toHaveProperty('title', 'Unauthorized');
+  });
 });
 
 describe('GET /users/:username', async () => {
   it('Get specific user succeeded', async () => {
-    const response = await request(app).get('/users/bob');
+    const response = await request(app)
+      .get('/users/bob')
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
@@ -103,7 +133,19 @@ describe('GET /users/:username', async () => {
   });
 
   it('Failed to get non-existent user', async () => {
-    const response = await request(app).get('/users/krish');
+    const response = await request(app)
+      .get('/users/krish')
+      .set({ Authorization: `Bearer ${bobToken}` });
+
+    const { error } = response.body;
+    expect(error.status).toBe(404);
+    expect(error).toHaveProperty('title', 'User Not Found');
+  });
+
+  it('Failed due to no auth token', async () => {
+    const response = await request(app)
+      .get('/users/krish')
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { error } = response.body;
     expect(error.status).toBe(404);
@@ -120,7 +162,8 @@ describe('PATCH /users/:username', async () => {
           name: 'bobbobman',
           password: 'abcdef'
         }
-      });
+      })
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
@@ -136,7 +179,8 @@ describe('PATCH /users/:username', async () => {
         user: {
           name: 'bobbobman'
         }
-      });
+      })
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
@@ -145,18 +189,19 @@ describe('PATCH /users/:username', async () => {
     expect(user).toHaveProperty('favorites');
   });
 
-  it('Failed to update non-existing user', async () => {
+  it('Failed to update other user details (not authorized)', async () => {
     const response = await request(app)
       .patch('/users/krish')
       .send({
         user: {
           name: 'bobbobman'
         }
-      });
+      })
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { error } = response.body;
-    expect(error.status).toBe(404);
-    expect(error).toHaveProperty('title', 'User Not Found');
+    expect(error.status).toBe(403);
+    expect(error).toHaveProperty('title', 'Forbidden');
   });
 
   it('Failed to update due to bad parameter', async () => {
@@ -167,17 +212,35 @@ describe('PATCH /users/:username', async () => {
           name: 'bobbobman',
           cookie: 'delicious'
         }
-      });
+      })
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { error } = response.body;
     expect(error.status).toBe(400);
     expect(error).toHaveProperty('title', 'Bad Request');
   });
+
+  it('Failed due to no auth token', async () => {
+    const response = await request(app)
+      .patch('/users/bob')
+      .send({
+        user: {
+          name: 'bobbobman',
+          cookie: 'delicious'
+        }
+      });
+
+    const { error } = response.body;
+    expect(error.status).toBe(401);
+    expect(error).toHaveProperty('title', 'Unauthorized');
+  });
 });
 
 describe('DELETE /users/:username', async () => {
   it('Deleting specific user succeeded', async () => {
-    const response = await request(app).delete('/users/bob');
+    const response = await request(app)
+      .delete('/users/bob')
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
@@ -188,18 +251,30 @@ describe('DELETE /users/:username', async () => {
     );
 
     // confirm users is deleted
-    const response2 = await request(app).get('/users/bob');
+    const response2 = await request(app)
+      .get('/users/bob')
+      .set({ Authorization: `Bearer ${bobToken}` });
     const { error } = response2.body;
     expect(error.status).toBe(404);
     expect(error).toHaveProperty('title', 'User Not Found');
   });
 
-  it('Failed to delete non-existent user', async () => {
-    const response = await request(app).delete('/users/jackrabbit');
+  it('Failed to delete other user', async () => {
+    const response = await request(app)
+      .delete('/users/jackrabbit')
+      .set({ Authorization: `Bearer ${bobToken}` });
 
     const { error } = response.body;
-    expect(error.status).toBe(404);
-    expect(error).toHaveProperty('title', 'User Not Found');
+    expect(error.status).toBe(403);
+    expect(error).toHaveProperty('title', 'Forbidden');
+  });
+
+  it('Failed due to no auth token', async () => {
+    const response = await request(app).delete('/users/bob');
+
+    const { error } = response.body;
+    expect(error.status).toBe(401);
+    expect(error).toHaveProperty('title', 'Unauthorized');
   });
 });
 
@@ -208,7 +283,10 @@ describe('POST /users/:username/favorites/:storyId', async () => {
     const stories = await Story.getStories({});
     const storyId = stories[1].storyId;
 
-    const response = await request(app).post(`/users/jas/favorites/${storyId}`);
+    const response = await request(app)
+      .post(`/users/jas/favorites/${storyId}`)
+      .set({ Authorization: `Bearer ${jasToken}` });
+
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
     expect(user).toHaveProperty('username', 'jas');
@@ -217,7 +295,10 @@ describe('POST /users/:username/favorites/:storyId', async () => {
   });
 
   it('Failed to add non-existent story', async () => {
-    const response = await request(app).post(`/users/jas/favorites/100000`);
+    const response = await request(app)
+      .post(`/users/jas/favorites/100000`)
+      .set({ Authorization: `Bearer ${jasToken}` });
+
     const { error } = response.body;
     expect(error.status).toBe(404);
     expect(error).toHaveProperty('title', 'Story Not Found');
@@ -227,12 +308,38 @@ describe('POST /users/:username/favorites/:storyId', async () => {
     const stories = await Story.getStories({});
     const storyId = stories[0].storyId;
 
-    const response = await request(app).post(`/users/jas/favorites/${storyId}`);
+    const response = await request(app)
+      .post(`/users/jas/favorites/${storyId}`)
+      .set({ Authorization: `Bearer ${jasToken}` });
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
     expect(user).toHaveProperty('username', 'jas');
     expect(user).toHaveProperty('favorites');
     expect(user.favorites).toHaveLength(1);
+  });
+
+  it('Failed due to trying to add to diff user favorites', async () => {
+    const stories = await Story.getStories({});
+    const storyId = stories[0].storyId;
+
+    const response = await request(app)
+      .post(`/users/jas/favorites/${storyId}`)
+      .set({ Authorization: `Bearer ${bobToken}` });
+
+    const { error } = response.body;
+    expect(error.status).toBe(403);
+    expect(error).toHaveProperty('title', 'Forbidden');
+  });
+
+  it('Failed due to no auth token', async () => {
+    const stories = await Story.getStories({});
+    const storyId = stories[0].storyId;
+
+    const response = await request(app).post(`/users/jas/favorites/${storyId}`);
+
+    const { error } = response.body;
+    expect(error.status).toBe(401);
+    expect(error).toHaveProperty('title', 'Unauthorized');
   });
 });
 
@@ -241,9 +348,10 @@ describe('DELETE /users/:username/favorites/:storyId', async () => {
     const stories = await User.getUserFavorites('jas');
     const storyId = stories[0].storyId;
 
-    const response = await request(app).delete(
-      `/users/jas/favorites/${storyId}`
-    );
+    const response = await request(app)
+      .delete(`/users/jas/favorites/${storyId}`)
+      .set({ Authorization: `Bearer ${jasToken}` });
+
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
     expect(user).toHaveProperty('username', 'jas');
@@ -252,7 +360,10 @@ describe('DELETE /users/:username/favorites/:storyId', async () => {
   });
 
   it('Failed to remove non-existent story', async () => {
-    const response = await request(app).delete(`/users/jas/favorites/100000`);
+    const response = await request(app)
+      .delete(`/users/jas/favorites/100000`)
+      .set({ Authorization: `Bearer ${jasToken}` });
+
     const { error } = response.body;
     expect(error.status).toBe(404);
     expect(error).toHaveProperty('title', 'Story Not Found');
@@ -262,14 +373,41 @@ describe('DELETE /users/:username/favorites/:storyId', async () => {
     const stories = await Story.getStories({});
     const storyId = stories[3].storyId;
 
-    const response = await request(app).delete(
-      `/users/jas/favorites/${storyId}`
-    );
+    const response = await request(app)
+      .delete(`/users/jas/favorites/${storyId}`)
+      .set({ Authorization: `Bearer ${jasToken}` });
+
     const { user } = response.body;
     expect(response.statusCode).toBe(200);
     expect(user).toHaveProperty('username', 'jas');
     expect(user).toHaveProperty('favorites');
     expect(user.favorites).toHaveLength(1);
+  });
+
+  it('Failed due to trying to remove from diff user favorites', async () => {
+    const stories = await Story.getStories({});
+    const storyId = stories[0].storyId;
+
+    const response = await request(app)
+      .delete(`/users/jas/favorites/${storyId}`)
+      .set({ Authorization: `Bearer ${bobToken}` });
+
+    const { error } = response.body;
+    expect(error.status).toBe(403);
+    expect(error).toHaveProperty('title', 'Forbidden');
+  });
+
+  it('Failed due to no auth token', async () => {
+    const stories = await Story.getStories({});
+    const storyId = stories[0].storyId;
+
+    const response = await request(app).delete(
+      `/users/jas/favorites/${storyId}`
+    );
+
+    const { error } = response.body;
+    expect(error.status).toBe(401);
+    expect(error).toHaveProperty('title', 'Unauthorized');
   });
 });
 
